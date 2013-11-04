8911131a49d2848aad3dc2dfb9fa2278da98ab9d
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-11-04 09:48:30 -0200

    Removing duplicated priority countries.

--- lib/country-select.rb
+++ lib/country-select.rb
@@ -7,23 +7,25 @@ module ActionView
       def country_select(object, method, priority_countries = nil, options = {}, html_options = {})
         country_options = []
 
+        cnt = COUNTRIES.dup
         if priority_countries && !priority_countries.empty?
-          if (unlisted = priority_countries - COUNTRIES.values).any?
+          if (unlisted = priority_countries - cnt.values).any?
             raise RuntimeError.new("Supplied priority countries are not in the main list: #{unlisted}")
           end
 
           sep = "-------------"
-          translated_priority_countries = translated_countries(priority_countries.map {|code| [COUNTRIES.key(code), code]})
+          translated_priority_countries = translated_countries(priority_countries.map {|code| [cnt.key(code), code]})
           country_options.unshift(sep)
           country_options.unshift(*translated_priority_countries.zip(priority_countries))
 
           disabled = Array.wrap(options.fetch(:disabled, []))
           disabled |= [sep]
           options[:disabled] = disabled
+          cnt.delete_if { |k,v| priority_countries.include? v }
         end
 
         Tags::Select.new(object, method, self, country_options + SortAlphabetical.sort(
-            translated_countries(COUNTRIES).zip(COUNTRIES.values)), options, html_options).render
+            translated_countries(cnt).zip(cnt.values)), options, html_options).render
       end
 
       def translated_countries(countries)
